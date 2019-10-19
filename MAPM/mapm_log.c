76746eeaaa7edf1e6c3f942b1ab54746085219c4
achan001 <albertmcchan@yahoo.com>
2019-10-18 23:45:22 -0400

    Add files via upload

--- MAPM/mapm_log.c
+++ MAPM/mapm_log.c
@@ -8,22 +8,23 @@ void M_apm_log(M_APM r, int places, M_APM x)
     return;
   }
 
-  int xexp = x->m_apm_exponent;  
-  int n = xexp - (xexp > 0);
-  if (!n) {M_log_basic_iteration(r, places, x); return;}
-  
+  unsigned xexp = x->m_apm_exponent;
+  if (xexp < 2U) {M_log_basic_iteration(r, places, x); return;}
+
   /* log (m * 10 ^ n) = log(m) + n * log(10) */
-  
+
   M_APM tmp0 = M_get_stack_var();
-  x->m_apm_exponent = xexp - n; /* log(m) and n same sign */
+  M_APM tmp1 = M_get_stack_var();
+  int base = (x->m_apm_data[0] < 32);
+  
+  x->m_apm_exponent = base;     /* 0.32 < m <= 3.2 */
   M_log_basic_iteration(tmp0, places, x);
   x->m_apm_exponent = xexp;     /* restore x */
 
-  M_APM tmp1 = M_get_stack_var();
-  m_apm_set_long(r, n);
+  m_apm_set_long(r, xexp - base);
   M_check_log_places(places);
   m_apm_multiply(tmp1, r, MM_lc_LOG_10);
-  
+
   m_apm_add(r, tmp0, tmp1);
   M_restore_stack(2);
 }
