85a1144ea341afd03af87436c69b794e9519c5aa
achan001 <albertmcchan@yahoo.com>
2019-11-13 18:16:51 -0500

    Add files via upload

--- MAPM/mapm_lg3.c
+++ MAPM/mapm_lg3.c
@@ -4,20 +4,26 @@
 
 static void M_2xAGM(M_APM r, int places, M_APM a, M_APM b)
 {
-    M_APM t = M_get_stack_var();
+    M_APM s = M_get_stack_var();
     places += 16;
-    int tolerance = -(places/2U);   // assumed r ~ 1
+    int tolerance = -(places/4U);   // assumed r ~ 1
 
     while (TRUE) {
-        m_apm_add(r, a, b);
-        m_apm_subtract(t, a, b);
-        if (t->m_apm_exponent <= tolerance) break;
-        m_apm_multiply(t, a, b);    // do AGM
-        m_apm_sqrt(b, places, t);
-        r->m_apm_exponent -= 2;
-        M_mul_digit(a, r, 50);
+        m_apm_add(s, a, b);
+        m_apm_subtract(r, a, b);
+        if (r->m_apm_exponent <= tolerance) break;
+        m_apm_multiply(r, a, b);    // do AGM
+        m_apm_sqrt(b, places, r);
+        s->m_apm_exponent -= 2;
+        M_mul_digit(a, s, 50);
         m_apm_iround(a, places);
     }
+    M_mul_digit(r, r, 50);          // 2*AGM = s - r^2/(4s)
+    r->m_apm_exponent -= 2;
+    m_apm_square(a, r);
+    places += a->m_apm_exponent;
+    m_apm_divide(b, places<0 ? 0: places, a, s);
+    m_apm_subtract(r, s, b);
     M_restore_stack(1);
 }
 
@@ -34,16 +40,18 @@ void M_check_log_places(int places)
 {
     if ((places += 6) <= MM_lc_log_digits) return;
 
-    MM_lc_log_digits = (places += 6);
-    int k = places/2U + 6;
-
     M_APM a = M_get_stack_var();
     M_APM b = M_get_stack_var();
     M_APM r = M_get_stack_var();
-    
-    m_apm_set_unsigned(a, k);
-    M_mul_digit(b, a, 4);
-    b->m_apm_exponent -= k;
+
+    MM_lc_log_digits = (places += 6);
+    unsigned n = places/4U + 4; // k = 2n to skip 1 AGM step
+
+    m_apm_set_unsigned(r, n);
+    M_mul_digit(b, r, 4);
+    b->m_apm_exponent -= n+n;
+    m_apm_add(a, r, b);         // a = (k + 4k/10^k)/2 = n + 4n/10^(2n)
+    b->m_apm_exponent += n;     // b = sqrt(4k^2/10^k) = 4n/10^n
     M_2xAGM(r, places, a, b);   // r = 1.36437635384 ...
 
     M_check_PI_places(places);
